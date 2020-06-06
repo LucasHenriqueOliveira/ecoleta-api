9eca2e8176ed40b90af7cde0ed7ab39d85112e26
Lucas <lucashen@gmail.com>
2020-06-06 12:04:37 -0300

    add controllers

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 import routes from './routes';
 
 const APP_PORT = 3333;
@@ -9,6 +10,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(APP_PORT, () => {
     console.log(`[*] Server running on port: ${APP_PORT}`);
